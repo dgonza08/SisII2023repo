41709680cd2b44c92deabf2b035ececd143abb31
dgonza08 <dgonza08@estudiantes.unileon.es>
2023-03-06 18:12:50 +0100

    Primera prueba conexion git

---
+++ SisII2023Repo/src/main/java/com/mycompany/sisii2023dos/main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.sisii2023dos;
+
+/**
+ *
+ * @author davic
+ */
+public class main {
+System.out.println("Hola mundo");
+
+}
