08df8a5ca6bbb969351a3cb545a4cb258c0a2dae
dgonza08 <dgonza08@estudiantes.unileon.es>
2023-03-10 23:53:06 +0100

    Subido codigo desde ordenador de sobremesa

--- SisII2023Repo/src/main/java/com/mycompany/sisii2023dos/main.java
+++ SisII2023Repo/src/main/java/com/mycompany/sisii2023dos/main.java
@@ -11,7 +11,8 @@ package com.mycompany.sisii2023dos;
  * @author davic
  */
 public class main {
-    int a=3;
-    System.out.println("Hola mundo aqui estamos : \n"+a);
-
+    public static void main(String[] args){
+        int a=3;
+        System.out.println("Hola mundo aqui estamos : \n"+a); 
+    }
 }
