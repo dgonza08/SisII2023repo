4ea65cd84cb04f9ece28901cb5ac1e6c5a058b4d
dgonza08 <dgonza08@estudiantes.unileon.es>
2023-03-13 14:36:44 +0100

    Subidos cambios en el main, estructura consulta

--- SisII2023Repo/src/main/java/com/mycompany/sisii2023dos/main.java
+++ SisII2023Repo/src/main/java/com/mycompany/sisii2023dos/main.java
@@ -6,13 +6,31 @@
 
 package com.mycompany.sisii2023dos;
 
+import java.util.Scanner;
+import org.hibernate.*;
+import java.util.List;
+
 /**
  *
  * @author davic
  */
 public class main {
+    private static SessionFactory sf;
     public static void main(String[] args){
-        int a=3;
-        System.out.println("Hola mundo aqui estamos : \n"+a); 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un DNI de un trabajador: ");
+        String dni = sc.nextLine();
+
+        Session sesion = sf.openSession();
+        String consultaDni = "SELECT * FROM trabajador n WHERE n.trabajador.NIFNIE=:param1";
+        Query query = sesion.createQuery(consultaDni); 
+        query.setParameter("param1", dni);
+        List lista = query.list();
+        
+        if(lista.isEmpty()){
+            System.out.println("No existe el trabajador con ese DNI");
+        }else{
+            System.out.println("El trabajador con ese DNI existe");
+        }
     }
 }
